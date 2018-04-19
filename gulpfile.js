70fb677de99125ac7335699f6f56aa7959f64694
Jon Arild Karlsen <karlsen.jonarild@gmail.com>
2018-04-19 11:51:06 +0200

    CHORE: implement gulp workflow/refactor css to scss

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+let gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    sourcemaps = require('gulp-sourcemaps'),
+    server = require('gulp-server-livereload');
+
+gulp.task('sass', () => {
+    gulp.src('scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(concat('styles.css'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('css/'));
+})
+
+gulp.task('watch', ['sass'], () => {
+    gulp.watch('scss/**/*.scss', ['sass']);
+})
+
+gulp.task('webserver', () => {
+    gulp.src('.')
+        .pipe(server({
+            livereload: true,
+            directoryListing: true,
+            open: true,
+            log: 'debug',
+            clientConsole: true
+        }));
+})
