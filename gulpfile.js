b95bad577f9c89faee56830d8a8c61b79d2908a6
Jon Arild Karlsen <karlsen.jonarild@gmail.com>
2018-04-20 11:09:58 +0200

    CHORE: improve gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,21 @@ let gulp = require('gulp'),
     sass = require('gulp-sass'),
     concat = require('gulp-concat'),
     sourcemaps = require('gulp-sourcemaps'),
-    server = require('gulp-server-livereload');
+    server = require('gulp-server-livereload'),
+    plumber = require('gulp-plumber'),
+    gutil = require('gulp-util'),
+    notify = require('gulp-notify');
 
 gulp.task('sass', () => {
     gulp.src('scss/**/*.scss')
+        .pipe(plumber({ errorHandler: (err) => {
+            notify.onError({
+                title: `Gulp error in ${err.plugin}`,
+                message: err.toString()
+            })(err)
+
+            gutil.beep();
+        }}))
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(concat('styles.css'))
@@ -23,6 +34,8 @@ gulp.task('webserver', () => {
             livereload: true,
             open: true,
             log: 'debug',
-            clientConsole: true
+            clientConsole: true,
         }));
 })
+
+gulp.task('default', ['sass', 'webserver', 'watch']);
